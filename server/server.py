19e4b03bf5a70754e9ca0d679d0f2bc74c584576
Eric <ericaltez1111@gmail.com>
2023-09-19 18:02:38 -0300

    changing branch

--- server/server.py
+++ server/server.py
@@ -2,8 +2,8 @@ import os
 from fastapi import FastAPI
 from database.database import Open, Close
 from config.config import Config
-from sqlalchemy import Connection, text, select
-
+from sqlalchemy import Connection, text, select, insert
+from models.Product import Product
 
 
 def setupServerRoutes(app:FastAPI):
@@ -11,15 +11,24 @@ def setupServerRoutes(app:FastAPI):
     async def root():
         return {"message": "Book store home page"}
 
-    @app.get("/users")
-    async def getAllUsers():
+    @app.get("/products")
+    async def getAllProducts():
         config = Config()
+        print("JOJOJO")
         pass
 
-    @app.post("/users")
-    async def createUser():
-        #Create a new user and save it in the database
-        pass
+    @app.post("/products")
+    async def createProducts():
+        #Create a new product and save it in the database
+        config = Config()
+        conn = Open(config.DbConfig)
+        name = "papa2"
+        price = 152
+        newproduct = Product(name, price)
+        print(newproduct)
+        conn.execute(insert(Product).values(name = name, price = price))
+        conn.commit()
+        Close(conn)
 
 def createServer():
     app = FastAPI()
