7e8c1c877086ecee4b30f898395cce8711427724
casmeyu <martin@casamayou.net>
2023-11-10 12:20:46 -0300

    MOD: Database module now exports a DB Class for instantiation
    Created DB Class to handle all the database actions (open, close and in the future abstraction of queries)

--- server/server.py
+++ server/server.py
@@ -1,11 +1,12 @@
 import os
 from fastapi import FastAPI
 from database.database import (
-    OpenConnection,
-    OpenSession,
-    CloseConnection,
-    CloseSession,
-    GetDatabaseTables
+    DB,
+    # OpenConnection,
+    # OpenSession,
+    # CloseConnection,
+    # CloseSession,
+    # GetDatabaseTables
 )
 from config.config import Config
 from sqlalchemy import Connection, text, select, insert
@@ -17,32 +18,39 @@ def setupServerRoutes(app:FastAPI):
     async def root():
         return {"message": "Book store home page"}
 
-    @app.get("/api/db/tables")
-    async def getDbTables():
-        config = Config()
-        session = OpenSession(config.DbConfig)
-        db_tables = GetDatabaseTables(session)
-        return db_tables
+    # @app.get("/api/db/tables")
+    # async def getDbTables():
+    #     config = Config()
+    #     session = OpenSession(config.DbConfig)
+    #     db_tables = GetDatabaseTables(session)
+    #     return db_tables
         
-    @app.get("/products")
-    async def getAllProducts():
-        config = Config()
-        print("JOJOJO")
-        pass
+    # @app.get("/products")
+    # async def getAllProducts():
+    #     config = Config()
+    #     print("JOJOJO")
+    #     pass
 
-    @app.post("/products")
-    async def createProducts():
-        #Create a new product and save it in the database
-        config = Config()
-        conn = OpenConnection(config.DbConfig)
-        name = "papa2"
-        price = 152
-        newproduct = Product(name, price)
-        print(newproduct)
-        conn.execute(insert(Product).values(name = name, price = price))
-        conn.commit()
-        CloseConnection(conn)
+    # @app.post("/products")
+    # async def createProducts():
+    #     #Create a new product and save it in the database
+    #     config = Config()
+    #     conn = OpenConnection(config.DbConfig)
+    #     name = "papa2"
+    #     price = 152
+    #     newproduct = Product(name, price)
+    #     print(newproduct)
+    #     conn.execute(insert(Product).values(name = name, price = price))
+    #     conn.commit()
+    #     CloseConnection(conn)
 
+    @app.get("/open")
+    async def open():
+        config = Config()
+        db = DB(config.DbConfig)
+        print(db.session)
+        db.CloseSession()
+        print(db.session.is_active)
 def createServer():
     app = FastAPI()
     setupServerRoutes(app)
