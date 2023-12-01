7a121592820b30b061bead909d0fac4f1a81a2da
Diekkan <merentiel.diego@gmail.com>
2023-12-01 16:20:55 -0300

    Added CORS configuration

--- server/server.py
+++ server/server.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config.config import Config
 from database.database import DB
 from datetime import datetime
 
+origins = [
+"*"
+]
+
 class Server():
+
     def __init__(self, config:Config):
         self.__config:Config = config
         self.db = DB(config.DbConfig)
         self.app:FastAPI = FastAPI()
         self.start:datetime = datetime.utcnow()
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins= origins,
+            allow_credentials= True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
     
