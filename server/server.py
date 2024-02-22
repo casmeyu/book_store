5c16108a2fe2edb18ecb743d2eb54f7201e36623
ericaltez <ericaltez1111@gmail.com>
2024-02-22 04:33:59 -0300

    The routers of each route are added to the app

--- server/server.py
+++ server/server.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI
 from config.config import Config
 from database.database import DB
 from datetime import datetime
+from routers.user_routes import setupUserRoutes
+from routers.product_routes import setupProductRoutes
+from routers.sale_routes import setupSaleRoutes
+from routers.role_routes import setupRoleRoutes
+
 
 class Server():
     def __init__(self, config:Config):
         self.__config:Config = config
         self.db = DB(config.DbConfig)
         self.app:FastAPI = FastAPI()
+        self.app.include_router(setupUserRoutes(self.db))
+        self.app.include_router(setupProductRoutes(self.db))
+        self.app.include_router(setupSaleRoutes(self.db))
+        self.app.include_router(setupRoleRoutes(self.db))
         self.start:datetime = datetime.utcnow()
+        
 
     
